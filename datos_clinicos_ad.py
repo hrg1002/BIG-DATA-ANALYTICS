d4eb1cb21fd8d7c14adae0ac3b713e9cba2ad558
hrg1002 <hrg1002@alu.ubu.es>
2024-11-12 18:07:18 +0100

    Primer commit con los datos clínicos y el código

---
+++ datos_clinicos_ad.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+archivo_excel = "C:/Users/hrond/OneDrive/Escritorio/Analytics/Atenciones.xlsx"
+df = pd.read_excel(archivo_excel)
+
+print(df.head())
+# Eliminar filas con valores nulos
+df = df.dropna()
+print(df.head())
