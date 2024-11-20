9ae548fd4fb03b23213c271c1fa1ea7a4286bd79
hrg1002 <hrg1002@alu.ubu.es>
2024-11-20 16:54:26 +0100

    Cambio a spark

--- datos_clinicos_ad.py
+++ datos_clinicos_ad.py
@@ -1,10 +1,32 @@
-import pandas as pd
+from pyspark.sql import SparkSession
+
+
+# To use it later with Spark, we have to create a session
+spark = SparkSession.builder \
+    .appName("Excel to Spark") \
+    .master("local") \
+    .config("spark.jars.packages", "com.crealytics:spark-excel_2.12:0.13.5") \
+    .getOrCreate()
 
 archivo_excel = "./Atenciones.xlsx"
-df = pd.read_excel(archivo_excel)
 
-print(df.head())
-# Eliminar filas con valores nulos
-df = df.dropna()
-print(df.head())
-print("p")
+spark_df = spark.read \
+    .format("com.crealytics.spark.excel") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load(archivo_excel)
+
+# Delete null lines
+spark_df = spark_df.dropna()
+
+# Delete duplicated lines
+spark_df = spark_df.dropDuplicates()
+
+# Show the first lines
+spark_df.show()
+
+# To save the DataFrame in Parquet format for its post process
+
+spark_df.write.parquet("clean_atenciones.parquet")
+
+spark.stop()
