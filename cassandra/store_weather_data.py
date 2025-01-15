3d28f46a5319be879616fbbf9608ee36cbdc4375
marc <martinezvallsmarc@gmail.com>
2025-01-15 17:00:41 +0100

    Refactor weather data storage and update Kafka DAG to include data insertion

--- cassandra/store_weather_data.py
+++ cassandra/store_weather_data.py
@@ -1,40 +1,43 @@
 from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
+def init() : 
 # Connect to the Cassandra cluster
-cluster = Cluster(['127.0.0.1'])
-session = cluster.connect()
+    auth_provider = PlainTextAuthProvider(username='cassandra', password='password')
+    cluster = Cluster(['127.0.0.1','9042'],auth_provider=auth_provider)
+    session = cluster.connect()
 
-# Create keyspace
-session.execute("""
-    CREATE KEYSPACE IF NOT EXISTS weather_data
-    WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
-""")
+    # Create keyspace
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS weather_data
+        WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
+    """)
 
-# Create table
-session.execute("""
-    CREATE TABLE IF NOT EXISTS weather_data.weather (
-        id UUID PRIMARY KEY,
-        city TEXT,
-        date DATE,
-        temperature FLOAT,
-        humidity FLOAT,
-        pressure FLOAT
-    )
-""")
+    # Create table
+    session.execute("""
+        CREATE TABLE IF NOT EXISTS weather_data.weather (
+            id UUID PRIMARY KEY,
+            date Date,
+            temperature FLOAT,
+            humidity FLOAT,
+            description TEXT
+        )
+    """)
+    return session,cluster
 
-# Function to insert processed weather data
+    # Function to insert processed weather data
 def insert_weather_data(weather_data):
-    insert_query = """
-        INSERT INTO weather_data.weather (id, city, date, temperature, humidity, pressure)
-        VALUES (uuid(), %s, %s, %s, %s, %s)
-    """
-    session.execute(insert_query, (
-        weather_data['city'],
-        weather_data['date'],
-        weather_data['temperature'],
-        weather_data['humidity'],
-        weather_data['pressure']
-    ))
+        session,cluster = init()
+        insert_query = """
+            INSERT INTO weather_data.weather (id, date, temperature, humidity,description)
+            VALUES (uuid(), %s, %s, %s, %s)
+        """
+        session.execute(insert_query, (
+            weather_data['fecha'],
+            weather_data['temperatura'],
+            weather_data['humedad'],
+            weather_data['descripcion']
+        ))
 
 # Function to retrieve and print weather data
 def retrieve_weather_data():
@@ -53,5 +56,5 @@ if __name__ == "__main__":
         'pressure': 1012.0
     }
     insert_weather_data(processed_weather_data)
-    retrieve_weather_data()
+    #retrieve_weather_data()
     # ...existing code...
--- dags/produce_consume_weather.py
+++ dags/produce_consume_weather.py
@@ -4,20 +4,34 @@
 This DAG will produce messages consisting of several elements to a Kafka cluster and consume
 them.
 """
+import pandas as pd
 import sys
 import os
 from airflow.decorators import dag
 from pendulum import datetime
 from airflow.providers.apache.kafka.operators.produce import ProduceToTopicOperator
 from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
+from airflow.operators.python_operator import PythonOperator
 
 # Add the ingestion directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../ingestion'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../cassandra'))
 
 from weather_producer import produce_weather_data
 from weather_consumer import get_weather_data 
+from store_weather_data import insert_weather_data, init
+
 NUMBER_OF_TREATS = 2
 KAFKA_TOPIC = 'weather_data'
+
+def store_data():
+    path = "./weather_data.parquet"
+    weather_data = pd.read_parquet(path,columns = ["temperatura","humedad","descripcion"])
+    weather_data['fecha'] = pd.Timestamp.today().date()
+    weather_dict = weather_data.iloc[0].to_dict()
+    print(weather_dict)
+    init()
+    insert_weather_data(weather_dict)
 
 @dag(
     start_date=datetime(2023, 4, 1),
@@ -45,7 +59,11 @@ def produce_consume_weather():
         max_batch_size=20,
     )
 
-    produce_weather_data_task >> consume_weather_data_task
+    store_weather_data_task = PythonOperator(
+        task_id='store_data',
+        python_callable=store_data
+    )
 
+    produce_weather_data_task >> consume_weather_data_task >> store_weather_data_task
 
 produce_consume_weather()
