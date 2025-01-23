276e237998580bc74cb359b07773507bf27bd327
marc <martinezvallsmarc@gmail.com>
2025-01-23 22:37:19 +0100

    tests

--- cassandra/generate_random.py
+++ cassandra/generate_random.py
@@ -3,30 +3,26 @@ import random
 import pandas as pd
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../cassandra'))
+from store_weather_data import insert_weather_data
+from store_pollution import insert_pollution_data
 
-def init_cassandra():
-    auth_provider = PlainTextAuthProvider(username='cassandra', password='password')
-    cluster = Cluster(['172.17.0.2', '9042'], auth_provider=auth_provider)
-    session = cluster.connect()
-    return session, cluster
-
-def generate_random_weather_data(num_records=10):
+def generate_random_data(num_records=20):
     weather_data = []
-    for _ in range(num_records):
-        record = {
-            'date': (datetime.now() - timedelta(days=random.randint(0, 365))).date(),
-            'temperature': random.uniform(-10, 40),
-            'humidity': random.uniform(0, 100),
-            'description': random.choice(['clear-sky', 'rain', 'snow', 'cloudy'])
-        }
-        weather_data.append(record)
-    return weather_data
-
-def generate_random_pollution_data(num_records=10):
     pollution_data = []
     for _ in range(num_records):
+        fecha = (datetime.now() - timedelta(days=random.randint(0, 365))).date()
         record = {
-            'date': (datetime.now() - timedelta(days=random.randint(0, 365))).date(),
+            'fecha': fecha,
+            'temperatura': random.uniform(-10, 40),
+            'humedad': random.uniform(0, 100),
+            'descripcion': random.choice(['clear-sky', 'rain', 'snow', 'cloudy'])
+        }
+        weather_data.append(record)
+        record = {
+            'date':fecha,
             'lat': random.uniform(-90, 90),
             'lon': random.uniform(-180, 180),
             'aqi': random.randint(0, 500),
@@ -40,51 +36,14 @@ def generate_random_pollution_data(num_records=10):
                 'pm10': random.uniform(0, 10),
                 'nh3': random.uniform(0, 10)
             }
-        }
-        pollution_data.append(record)
-    return pollution_data
-
-def insert_weather_data(session, weather_data):
-    insert_query = """
-        INSERT INTO weather_data.weather (id, date, temperature, humidity, description)
-        VALUES (uuid(), %s, %s, %s, %s)
-    """
-    for data in weather_data:
-        session.execute(insert_query, (
-            data['date'],
-            data['temperature'],
-            data['humidity'],
-            data['description']
-        ))
-
-def insert_pollution_data(session, pollution_data):
-    insert_query = """
-        INSERT INTO pollution_data.pollution (id, date, lat, lon, aqi, co, no, no2, o3, so2, pm2_5, pm10, nh3)
-        VALUES (uuid(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-    for data in pollution_data:
-        session.execute(insert_query, (
-            data['date'],
-            data['lat'],
-            data['lon'],
-            data['aqi'],
-            data['pollution']['co'],
-            data['pollution']['no'],
-            data['pollution']['no2'],
-            data['pollution']['o3'],
-            data['pollution']['so2'],
-            data['pollution']['pm2_5'],
-            data['pollution']['pm10'],
-            data['pollution']['nh3']
-        ))
-
+        }  
+        pollution_data.append(record)        
+    print(pollution_data)
+    return weather_data,pollution_data
+        
 if __name__ == "__main__":
-    session, cluster = init_cassandra()
     
-    weather_data = generate_random_weather_data(num_records=10)
-    insert_weather_data(session, weather_data)
-    
-    pollution_data = generate_random_pollution_data(num_records=10)
-    insert_pollution_data(session, pollution_data)
-    
-    cluster.shutdown()
+    weather_data , pollution_data = generate_random_data(50)
+    for i in range(len(weather_data)) :
+        insert_weather_data(weather_data[i])
+        insert_pollution_data(pollution_data[i])
